3c5a2ff9f6ef86d9691a33ea5439b6f0270b1dd0
Babuna-0501 <74554961+Babuna-0501@users.noreply.github.com>
2023-11-12 00:01:35 +0800

    fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,21 +5,24 @@
           <h6>Гэрэлт Цох Байшин ТББ</h6>
           <p>gerelt.tsokhb@tokhiruulga.mn</p>
         </div>
-        <div class="sec">
-          <h6>Tokhiruulga.mn</h6>
-          <p>Талархал</p>
-          <p>Товч танилцуулга</p>
-          <p>Зохиогчийн эрхийн мэдэгдэл</p>
-          <p>Үйлдэл гүйцэтгэх чадвар алдалт</p>
-          <p>Халуун сэдвүүд</p>
+        <div class="mid">
+          <div class="sec">
+            <h6>Tokhiruulga.mn</h6>
+            <p>Талархал</p>
+            <p>Товч танилцуулга</p>
+            <p>Зохиогчийн эрхийн мэдэгдэл</p>
+            <p>Үйлдэл гүйцэтгэх чадвар алдалт</p>
+            <p>Халуун сэдвүүд</p>
+          </div>
+          <div class="sec">
+            <p>Хуулийн мэдээллийн булан</p>
+            <p>Нэмэлт сургалт ба зөвлөгөө мэдээлэл</p>
+            <p>Ханган нийлүүлэгч</p>
+            <p>Санал сэтгэгдэл</p>
+            <p>Холбоо барих</p>
+          </div>
         </div>
-        <div>
-          <p>Хуулийн мэдээллийн булан</p>
-          <p>Нэмэлт сургалт ба зөвлөгөө мэдээлэл</p>
-          <p>Ханган нийлүүлэгч</p>
-          <p>Санал сэтгэгдэл</p>
-          <p>Холбоо барих</p>
-        </div>
+  
       </div>
       <div class="btm">
         <div><h6>© 2023 Гэрэлт Цох Байшин ТББ. Бүх эрх хуулиар хамгаалагдсан.</h6></div>
@@ -42,7 +45,7 @@ export default {
 
   .footer {
     width: 100%;
-    height: 412px;
+    height: auto;
     background-color: var(--success-color);
     display: flex;
     flex-direction: column;
@@ -52,21 +55,30 @@ export default {
 
   .footer h6 {
     font-weight: bold;
+    margin-bottom: 50px;
+  }
+
+  .footer p {
+   line-height: 20px;
   }
   .top {
     display: inherit;
     flex-direction: row;
     align-items: baseline;
-    justify-content: space-between;
+    gap: 430px;
   }
 
-  .top .sec {
-    margin-left: 260px;
-  }
   .btm {
     display: inherit;
     flex-direction: row;
-    justify-content: space-between;
+    gap: 700px;
+  }
+
+  .mid {
+    display: flex;
+    flex-direction: row;
+    align-items:end;
+    gap: 90px;
   }
 
   .btm .social {
